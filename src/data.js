95cc8521bb0c2f96c42681e0f83738656345103d
mussu777 <mustak65ee@gmail.com>
2023-10-14 23:39:04 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,50 @@
+import Amit from './Assets/amit3.jpeg'
+import Dwipanjan from './Assets/dwipanjan.jpg'
+import Kaushik from './Assets/kaushik.jpg'
+import Nayan from './Assets/nayan.jpg'
+import Swastik from './Assets/swastik.jpeg'
+
+const reviews = [
+  {
+    id: 1,
+    name: "Amit Mandal",
+    job: "Model/Actor",
+    image: `${Amit}`,
+    alt: "Amit Mandal - Model/Actor",
+    text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their brilliant expertise and ability to deliver quality scenes on time and within budget. They are a true partner and an asset to any stuffs.",
+  },
+  {
+    id: 2,
+    name: "Dwipanjan Singh",
+    job: "ECE Core Engineer",
+    image: `${Dwipanjan}`,
+    alt: "Dwipanjan Singh - ECE Core Engineer",
+    text: "I have been working with this company for several years, and I have always been impressed with their creativity & attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+  },
+  {
+    id: 3,
+    name: "Kaushik Kumar",
+    job: "Data Analyst",
+    image: `${Kaushik}`,
+    alt: "Kaushik Kumar - Data Analyst",
+    text: "I am thrilled with the results of our recent project, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
+  },
+  {
+    id: 4,
+    name: "Nayan Singh Sardar",
+    job: "Content Strategist",
+    image: `${Nayan}`,
+    alt: "Nayan Singh Sardar - Content Strategist",
+    text: "I have worked with many content creators over the years, but none have impressed me as much as this team. They have a knack for crafting compelling and engaging content that resonates with our audience and drives real results. I highly recommend them!",
+  },
+  {
+    id: 5,
+    name: "Swastik Bhattacharya",
+    job: "Developer/Coder",
+    image: `${Swastik}` ,
+    alt: "Swastik Bhattacharya - Developer/Coder",
+    text: "I had the opportunity to work with this team on a Web Dev project, and I was impressed with their attention to detail and their ability to work with complex coding. They provided valuable insights that helped us make informed Developing decisions. It was a pleasure working with them.",
+  },
+];
+
+export default reviews;
